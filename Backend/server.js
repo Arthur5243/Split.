be6c967d5abb16b3fc16fd982c7ee60d6bb731f7
Arthur5243 <arthur.cambin@gmail.com>
2026-08-08 23:41:47 +0200

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import oddsRouter from "./odds.js";
 
 const app = express();
 app.use(cors());
+app.use(oddsRouter);
 
 const PORT = process.env.PORT || 3000;
 const PANDASCORE_API_KEY = process.env.PANDASCORE_API_KEY;
